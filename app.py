31fd6d5b800ed36e805ae15c3e2a319ac96a00b8
Tom McEvoy <60655605+162tmac@users.noreply.github.com>
2021-04-25 14:00:52 +0100

    Updated templates front end to include register and login

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-app.config.update(TEMPLATES_AUTO_RELOAD = True)
+app.config.update(TEMPLATES_AUTO_RELOAD=True)
 
 mongo = PyMongo(app)
 
@@ -25,12 +26,12 @@ def index():
 
 @app.route("/login")
 def login():
-    return ''
+    return render_template('login.html')
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return ''
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
